3ada85a2c0c28ead5652ed4e3ad11909540790ea
Chirita Andrei <andrei.chirita2013@gmail.com>
2021-03-04 16:29:42 +0200

    Creare clase Reader

--- Seminar2/src/ro/ase/cts/program/Program.java
+++ Seminar2/src/ro/ase/cts/program/Program.java
@@ -2,17 +2,21 @@ package ro.ase.cts.program;
 
 import java.io.FileNotFoundException;
 import ro.ase.cts.clase.Angajat;
-import ro.ase.cts.clase.Utils;
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.clase.readers.AngajatReader;
+
 import java.util.List;
-import ro.ase.cts.clase.Utils;
 
 public class Program {
+	
+	//nu a fost respectat Dependency Inversion 
+	//sa incerci sa nu depinzi de o clasa concreta ci de tipul abstract
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> listaAngajati; 
 		try {
-			listaAngajati = Utils.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
+			listaAngajati = AngajatReader.readAngajati("angajati.txt");
+			for(Aplicant angajat:listaAngajati)
 				System.out.println(angajat.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
